b354e6c774aaf788cd69ae4c99be638b0c6ac018
RexHuang <a527084702@gmail.com>
2022-03-25 22:02:37 +1100

    basic js for the button done.
    update for link the page with the button. So when click the button and switch into different sections.

--- app.js
+++ app.js
@@ -16,6 +16,31 @@ function PageTransitions()
         })
     }
 
+    //section active class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id)
+        {
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
+/* Test event
+allSections.addEventListener('click', (e) => {
+        console.log(e.target);
+    }) 
+*/
 PageTransitions();
